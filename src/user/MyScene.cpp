38a5a1358e025f76161cf10bdecd406338a606ac
Oranges-sour <873516725@qq.com>
2025-01-04 23:54:14 +0800

    1

--- src/user/MyScene.cpp
+++ src/user/MyScene.cpp
@@ -16,13 +16,12 @@ using namespace std;
 using namespace std::chrono;
 
 void MyScene::init() {
-    this->add_item(make_shared<Text>("图书管理信息系统", 3, 1));
+    this->add_item(make_shared<Text>("图书管理信息系统", LINES / 2, 1));
 
     this->add_item(make_shared<Button>(
         "Start", 3, 3, [](shared_ptr<Button> _btn) { _btn->label += "a"; }));
     this->add_item(
         make_shared<Button>("Quit", 3, 5, [](shared_ptr<Button>) {}));
-    this->add_item(make_shared<Text>("Hello", 3, 1));
     this->add_item(make_shared<Input>(
         "ISBN号", 3, 7, 15,
         [](shared_ptr<Input> _input, const string& str) { _input->x += 1; }));
